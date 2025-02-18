825ce5790f9dc47c45dbb0f1ae2dd3bd3d2d00e5
MatisseBro <132852961+MatisseBro@users.noreply.github.com>
2025-02-18 15:47:59 +0100

    api

--- front/Invoiceflow-angular/src/app/app.routes.ts
+++ front/Invoiceflow-angular/src/app/app.routes.ts
@@ -3,11 +3,13 @@ import { LoginComponent } from './components/login/login.component';
 import { ProduitComponent } from './produit/produit.component';
 import { LandingPageComponent } from './components/landing-page/landing-page.component';
 import { FactureComponent } from './facture/facture.component';
+import { ClientComponent } from './client/client.component';
 
 
 export const routes: Routes = [
     { path: 'login', component: LoginComponent },
     { path: 'produit', component: ProduitComponent },
     { path: 'facture', component: FactureComponent },
+    { path: 'client', component: ClientComponent },
     { path: '', component: LandingPageComponent },
   ];
